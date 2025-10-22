508982cb05494e1f6d7d7628aa63a3f6821d418f
Odianosen Osose <odianosen.osose@pau.edu.ng>
2025-10-22 19:07:40 +0100

    (Using Standard Input in Rust)

--- week-4/practice-1/src/main.rs
+++ week-4/practice-1/src/main.rs
@@ -13,17 +13,10 @@ io::stdin()
 println!("Your name is: {}", name);
 
 //input age
-<<<<<<< HEAD
 println!("\nPlease Enter your age.");
-=======
-println!("\n Please Enter your age.");
->>>>>>> d6af629b7a40724e32fd4c54b5211b65acc4a00c
 let mut age = String::new();
 io::stdin().read_line(&mut age).expect("Failed to read input");
 let age:u8 = age.trim().parse().expect("Input not an integer");
 println!("Your age is: {}", age);
-<<<<<<< HEAD
+
 }
-=======
-}
->>>>>>> d6af629b7a40724e32fd4c54b5211b65acc4a00c
