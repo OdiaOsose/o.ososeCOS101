354353995b148839bcedf681ab455d5eec3618d7
Odianosen Osose <odianosen.osose@pau.edu.ng>
2025-11-05 17:07:55 +0100

    (Using Arithmetic Operators in Rust)

---
+++ week-6/practice-8/target/debug/deps/practice_8.d
@@ -0,0 +1,5 @@
+C:\Users\hp\Documents\o.ososeCOS101\week-6\practice-8\target\debug\deps\practice_8.d: src\main.rs
+
+C:\Users\hp\Documents\o.ososeCOS101\week-6\practice-8\target\debug\deps\practice_8.exe: src\main.rs
+
+src\main.rs:
