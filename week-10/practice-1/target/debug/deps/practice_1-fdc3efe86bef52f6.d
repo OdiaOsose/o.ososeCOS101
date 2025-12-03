717d54e9f1ab14c91905de9cade994f3d10d47f8
Odianosen Osose <odianosen.osose@pau.edu.ng>
2025-12-03 20:55:19 +0100

    Assigning Variable Value to Another Variable

---
+++ week-10/practice-1/target/debug/deps/practice_1-fdc3efe86bef52f6.d
@@ -0,0 +1,5 @@
+C:\Users\hp\Documents\week-10\practice-1\target\debug\deps\practice_1-fdc3efe86bef52f6.d: src\main.rs
+
+C:\Users\hp\Documents\week-10\practice-1\target\debug\deps\libpractice_1-fdc3efe86bef52f6.rmeta: src\main.rs
+
+src\main.rs:
