8fa4669ae40dde6e7248ca961652be7a3df78756
Odianosen Osose <odianosen.osose@pau.edu.ng>
2025-12-03 21:00:03 +0100

    Passing Value to a Function Example

---
+++ week-10/practice-4/target/debug/deps/practice_4-e50a7b4f5f37dd58.d
@@ -0,0 +1,5 @@
+C:\Users\hp\Documents\week-10\practice-4\target\debug\deps\practice_4-e50a7b4f5f37dd58.d: src\main.rs
+
+C:\Users\hp\Documents\week-10\practice-4\target\debug\deps\libpractice_4-e50a7b4f5f37dd58.rmeta: src\main.rs
+
+src\main.rs:
