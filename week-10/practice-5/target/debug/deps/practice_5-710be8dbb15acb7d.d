da5b47faa6fb63cec87cde7fa63c91ac92c6e3cd
Odianosen Osose <odianosen.osose@pau.edu.ng>
2025-12-03 21:00:47 +0100

    Borrowing in Rust

---
+++ week-10/practice-5/target/debug/deps/practice_5-710be8dbb15acb7d.d
@@ -0,0 +1,5 @@
+C:\Users\hp\Documents\week-10\practice-5\target\debug\deps\practice_5-710be8dbb15acb7d.d: src\main.rs
+
+C:\Users\hp\Documents\week-10\practice-5\target\debug\deps\libpractice_5-710be8dbb15acb7d.rmeta: src\main.rs
+
+src\main.rs:
