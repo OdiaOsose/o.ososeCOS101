a72f213a5b7c4525c8e4f1a8c50da1f4bf65b730
Odianosen Osose <odianosen.osose@pau.edu.ng>
2025-12-03 20:57:39 +0100

    Returning Value from a Function

---
+++ week-10/practice-3/target/debug/deps/practice_3-df925dfac850e4c2.d
@@ -0,0 +1,5 @@
+C:\Users\hp\Documents\week-10\practice-3\target\debug\deps\practice_3-df925dfac850e4c2.d: src\main.rs
+
+C:\Users\hp\Documents\week-10\practice-3\target\debug\deps\libpractice_3-df925dfac850e4c2.rmeta: src\main.rs
+
+src\main.rs:
