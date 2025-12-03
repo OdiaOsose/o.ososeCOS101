fb7e241e055ac5e6bbecc353f71ad31f7d0831e1
Odianosen Osose <odianosen.osose@pau.edu.ng>
2025-12-03 21:02:28 +0100

    Structs in Rust

---
+++ week-10/practice-7/target/debug/deps/practice_7-a5857abd33e8bdb5.d
@@ -0,0 +1,5 @@
+C:\Users\hp\Documents\week-10\practice-7\target\debug\deps\practice_7-a5857abd33e8bdb5.d: src\main.rs
+
+C:\Users\hp\Documents\week-10\practice-7\target\debug\deps\libpractice_7-a5857abd33e8bdb5.rmeta: src\main.rs
+
+src\main.rs:
