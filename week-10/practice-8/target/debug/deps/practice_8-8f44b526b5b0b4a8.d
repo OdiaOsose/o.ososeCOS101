de1ec646d7a7fd558bfa2587289b4156b96f9ae0
Odianosen Osose <odianosen.osose@pau.edu.ng>
2025-12-03 21:03:32 +0100

    Passing a Struct to a Function

---
+++ week-10/practice-8/target/debug/deps/practice_8-8f44b526b5b0b4a8.d
@@ -0,0 +1,5 @@
+C:\Users\hp\Documents\week-10\practice-8\target\debug\deps\practice_8-8f44b526b5b0b4a8.d: src\main.rs
+
+C:\Users\hp\Documents\week-10\practice-8\target\debug\deps\libpractice_8-8f44b526b5b0b4a8.rmeta: src\main.rs
+
+src\main.rs:
