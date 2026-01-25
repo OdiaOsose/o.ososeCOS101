146c9b644f2777706c8417ea083fc0ab06aef0a6
Odianosen Osose <odianosen.osose@pau.edu.ng>
2026-01-25 16:33:24 +0100

    PostgreSQL and Rust Project

---
+++ week-13/practice/target/debug/deps/practice.d
@@ -0,0 +1,5 @@
+C:\Users\hp\Documents\o.ososeCOS101\week-13\practice\target\debug\deps\practice.d: src\main.rs
+
+C:\Users\hp\Documents\o.ososeCOS101\week-13\practice\target\debug\deps\practice.exe: src\main.rs
+
+src\main.rs:
