1d1b0370742edb6cb01a5aa63076be6fbcba4b80
Odianosen Osose <odianosen.osose@pau.edu.ng>
2026-01-25 16:29:38 +0100

    PostgreSQL and Rust Project

---
+++ week-13/project/psql_rust_project/target/debug/deps/psql_rust_project-666926342d4b0d1a.d
@@ -0,0 +1,5 @@
+C:\Users\hp\Documents\o.ososeCOS101\week-13\project\psql_rust_project\target\debug\deps\psql_rust_project-666926342d4b0d1a.d: src\main.rs
+
+C:\Users\hp\Documents\o.ososeCOS101\week-13\project\psql_rust_project\target\debug\deps\libpsql_rust_project-666926342d4b0d1a.rmeta: src\main.rs
+
+src\main.rs:
---
+++ week-13/project/psql_rust_project/target/debug/psql_rust_project.d
@@ -0,0 +1,1 @@
+C:\Users\hp\Documents\o.ososeCOS101\week-13\project\psql_rust_project\target\debug\psql_rust_project.exe: C:\Users\hp\Documents\o.ososeCOS101\week-13\project\psql_rust_project\src\main.rs
